2128f8fe7eb03160b6d87a097bc9a921534315ba
Paty G <patyshagulg@gmail.com>
2021-02-05 13:33:36 -0600

    First commit

---
+++ InterviewQuestions/ArrayDublicates.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InterviewQuestions
+{
+    class ArrayDublicates
+    {
+
+        // create an array and return boolean if the array has dublicates or not
+        public static void Main(string[] args)
+        {
+            int[] nums1 = { 1, 2, 5, 7, 8 };
+            Console.WriteLine("Let's see does the first array has dublicates");
+            Console.Write(ArrayDub(nums1));
+
+            Console.WriteLine();
+
+            int[] nums2 = { 1, 2, 4, 7 };
+            Console.WriteLine("Let's see does the second array has dublicates");
+            Console.Write(ArrayDub(nums2));
+
+            Console.ReadLine();
+
+        }
+
+
+        public static bool ArrayDub(int[] nums)
+        {
+            bool dublicate = true;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && i != j)
+                    {
+                        dublicate = false;
+                        
+                    }
+                
+                }
+                    
+            
+            }
+            return dublicate;
+        }
+        
+    }
+}
---
+++ InterviewQuestions/Fibon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestions
+{
+    class Fibon
+    {
+
+        // create a method that will print first declared fibonnaci numbers
+        static void Main(string[] args)
+        {
+            Fibonnaci1(5);
+
+            Console.WriteLine();
+
+            Fibonnaci2(7);
+            Console.ReadLine();
+        }
+
+        public static void Fibonnaci1(int num) // in the parameters we will put the number that we want to print
+        {
+
+            int a = 0;
+            int b = 1;
+            int c;
+
+            Console.Write(a + " " + b + " ");
+
+            for (int i = 2; i < num; i++)
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+
+            }
+        
+        }
+
+
+        public static void Fibonnaci2(int num)
+        {
+            int a = 0;
+            int b = 1;
+            int c; 
+
+            for (int i = 0; i < num; i++)
+            {
+
+                c = a + b;
+                Console.Write(a + " ");
+                a = b;
+                b = c;
+            
+            }
+        }
+    }
+}
