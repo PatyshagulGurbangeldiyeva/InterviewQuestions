6af7bd6d660351f6e557f2dcfff2a981d9044479
Paty G <patyshagulg@gmail.com>
2021-02-08 14:58:56 -0600

    finishes FizzBuzz

--- InterviewQuestions/FizzBuzz.cs
+++ InterviewQuestions/FizzBuzz.cs
@@ -8,15 +8,41 @@ namespace InterviewQuestions
 {
     class FizzBuzz
     {
-        // kjdlskdjflksdjflkjd
+        /** 
+        Print the numbers 1..100
+        For multiples of 3, print "Fizz" instead of the number
+        For multiples of 5, print "Buzz" instead of the number
+        For multiples of 3 and 5, print "FizzBuzz" instead of the number
+        
+    **/
         public static void Main(string[] args)
         {
-            Console.WriteLine(FizzBuzz);
+            FizBuz (20);
+            Console.ReadLine();
         }
 
-        public static void FizzBuzz()
+        public static void FizBuz(int num)
         { 
+            for (int i=1; i<=num; i++) 
+            {
+                if (i % 3 == 0 && i %5 !=0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0 && i % 3 !=0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizBuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
 
+                }
+            }
 
         }
 
