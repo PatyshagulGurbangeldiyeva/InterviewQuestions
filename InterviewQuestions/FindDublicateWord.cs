087b7a0e74ea2579fb8c4949a8151705e839acbf
Paty G <patyshagulg@gmail.com>
2021-02-10 14:27:56 -0600

    Dublicate Words

---
+++ InterviewQuestions/FindDublicateWord.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestions
+{
+    class FindDublicateWord
+    {
+        public static void Main(string[] args)
+        {
+            string str = "Hello and Hello my name is John and my Last name is JohnJohn";
+            DublictateWord(str);
+            Console.ReadLine();
+        }
+
+        public static void DublictateWord(string str)
+        {
+            string [] s = str.Split();
+
+            for (int i=0; i<s.Length; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i] == s[j] && i != j)
+                    {
+                        Console.WriteLine(s[i]);
+                    }
+                    
+                }
+         
+            }
+          
+        }
+
+    }
+}
